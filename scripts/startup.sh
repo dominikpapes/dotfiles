e9ee0bd430e3aa51381e61adb86abf75430de742
Dominik <dmnkpapes@gmail.com>
2026-04-21 21:14:33 +0200

    Add icons

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -62,4 +62,6 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 # Apps
 dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda mpv neovim gnome-tweaks papirus-icon-theme autojump
 flatpak install -y flathub com.mattjakeman.ExtensionManager
+flatpak install -y flathub com.github.tchx84.Flatseal
+flatpak install -y flathub com.mastermindzh.tidal-hifi
 
